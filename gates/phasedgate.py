15acae701c187a67f11a1fcfdb7109bd0a86a0d3
Kshitiz Anand <subodhkshitiz1971@gmail.com>
2022-11-12 18:42:57 +0530

    changed naming conventions

--- gates/phasedgate.py
+++ gates/phasedgate.py
@@ -4,41 +4,41 @@ class phased_gate:
 
     def p_gate(qubit_state, angle):
         a = np.exp(complex(0,angle))
-        operator = [[1,0],[0,a]]
-        output = np.dot(operator, qubit_state)
+        p_operator = [[1,0],[0,a]]
+        output = np.dot(p_operator, qubit_state)
         return output
 
     def virtual_z_gate(qubit_state):
         a = np.exp(complex(0, np.pi))
-        operator = [[1,0],[0,a]]
-        output = np.dot(operator, qubit_state)
+        virtual_z_operator = [[1,0],[0,a]]
+        output = np.dot(virtual_z_operator, qubit_state)
         return output
 
     def i_gate(qubit_state):
-        operator = [[1,0],[0,1]]
-        output = np.dot(operator, qubit_state)
+        i_operator = [[1,0],[0,1]]
+        output = np.dot(i_operator, qubit_state)
         return output
 
     def s_gate(qubit_state):
         a = np.exp(complex(0, np.pi/2))
-        operator = [[1,0],[0,a]]
-        output = np.dot(operator, qubit_state)
+        s_operator = [[1,0],[0,a]]
+        output = np.dot(s_operator, qubit_state)
         return output
     
     def t_gate(qubit_state):
         a = np.exp(complex(0, np.pi/4))
-        operator = [[1,0],[0,a]]
-        output = np.dot(operator, qubit_state)
+        t_operator = [[1,0],[0,a]]
+        output = np.dot(t_operator, qubit_state)
         return output
 
     def s_dagger_gate(qubit_state):
         a = np.exp(complex(0, -np.pi/2))
-        operator = [[1,0],[0,a]]
-        output = np.dot(operator, qubit_state)
+        s_dagger_operator = [[1,0],[0,a]]
+        output = np.dot(s_dagger_operator, qubit_state)
         return output
 
     def t_dagger_gate(qubit_state):
         a = np.exp(complex(0, -np.pi/4))
-        operator = [[1,0],[0,a]]
-        output = np.dot(operator, qubit_state)
+        t_dagger_operator = [[1,0],[0,a]]
+        output = np.dot(t_dagger_operator, qubit_state)
         return output  
